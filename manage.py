17fbe6beae3fef1687ee3fa7484ddbe1f6fd17ea
Chris Rowles <me@rowles.ch>
2021-02-15 23:50:22 +0000

    add supervisord scripts

--- manage.py
+++ manage.py
@@ -2,16 +2,15 @@ from tornado.options import define, options
 from tornado.ioloop import IOLoop
 from app.main import create_app
 
+define('port', default=8000, help='listen port', type=int)
+
 app = create_app()
 
-define('address', default='0.0.0.0', help='listen address')
-define('port', default=4200, help='listen port', type=int)
 
-
-def run():
-    app.listen(options.port, options.address)
-    IOLoop.current().start()
+def main():
+    app.listen(options.port)
+    IOLoop.instance().start()
 
 
 if __name__ == '__main__':
-    run()
+    main()
